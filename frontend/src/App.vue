e3ee0a611415c7aaa781df91b4f875e6761bb31b
Kit-Kat31415926 <ravenkc2017@gmail.com>
2025-10-27 22:08:57 -0700

    Update main UI

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Main from './components/Main.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    	<Main />
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
@@ -22,26 +13,15 @@ header {
   line-height: 1.5;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+header {
+	display: flex;;
+	justify-content: center;
+	align-items: center;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+header .wrapper {
+	display: flex;
+	place-items: center;
+	flex-wrap: wrap;
 }
 </style>
